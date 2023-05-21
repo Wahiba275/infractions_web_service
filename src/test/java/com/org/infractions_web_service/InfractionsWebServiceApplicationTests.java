75dfc02e6e39fcdf9e854574d9a99d4912fc64b4
Wahiba275 <123891320+Wahiba275@users.noreply.github.com>
2023-05-21 10:48:20 +0000

    Initial commit

---
+++ src/test/java/com/org/infractions_web_service/InfractionsWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.infractions_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfractionsWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
